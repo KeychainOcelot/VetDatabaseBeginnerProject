c4837699dc66e14c47527bd1936d7fc90d1d199e
Shunal Doke <mysticshunal@gmail.com>
2022-05-22 20:45:09 +0530

    classes in progress

--- VetDatabaseProject/Patients.cpp
+++ VetDatabaseProject/Patients.cpp
@@ -1,1 +1,47 @@
 #include "Patients.h"
+#include <iostream>
+using namespace std;
+
+Patients::Patients(string ownerName, string petName, string petSpecies)
+{
+    this->ownerName = ownerName;
+    this->petName = petName;
+    this->petSpecies = petSpecies;
+}
+void Patients::setHistory(string petHistoryDisease, string petHistoryMedicine, bool petNeut)
+{
+    this->petHistoryDisease = petHistoryDisease;
+    this->petHistoryMedicine = petHistoryMedicine;
+    this->petNeut = petNeut;
+
+}
+void Patients::setVaccine(string petVaccine, string petVaccineDate)
+{
+    this->petVaccine = petVaccine;
+    this->petVaccineDate = petVaccineDate;
+
+}
+void Patients::setPetDetails(string petBreed, int petAge, double petWeight)
+{
+    this-> petBreed = petBreed;
+    this->petAge = petAge;
+    this->petWeight = petWeight;
+
+}
+void Patients::getOwnerContact() const
+{
+    cout<<"Owner's name: "<<ownerName<<endl;
+    cout<<"Owner's contact: "<<ownerContact<<endl;
+}
+void Patients::getPetDetails() const
+{
+
+}
+void Patients::getPetHistory() const
+{
+
+}
+void Patients::getOwnerDetails() const
+{
+
+}
--- VetDatabaseProject/Patients.h
+++ VetDatabaseProject/Patients.h
@@ -1,4 +1,33 @@
 #ifndef PATIENTS_H
 #define PATIENTS_H
+#include <string>
+#include <vector>
+using namespace std;
+
+class Patients{
+    public:
+        Patients(string ownerName, string petName, string petSpecies);
+        void setHistory(string petHistoryDisease, string petHistoryMedicine, bool petNeut);
+        void setVaccine(string petVaccine, string petVaccineDate);
+        void setPetDetails(string petBreed, int petAge, double petWeight);
+        string getOwnerContact() const;
+        void getPetDetails() const;
+        void getPetHistory() const;
+        void getOwnerDetails() const;
+
+    private:
+        string ownerName;
+        int ownerContact;
+        string petName;
+        string petSpecies;
+        string petBreed;
+        int petAge;
+        double petWeight;
+        bool petNeut;
+        vector<string> petVaccine;        
+        vector<string> petVaccineDate;
+        string petHistoryDisease;
+        string petHistoryMedicine;
+};
 
 #endif
