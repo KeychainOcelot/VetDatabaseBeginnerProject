3f63c2fc1f98561f47d685bded665b223793716c
Shunal Doke <mysticshunal@gmail.com>
2022-05-29 03:42:05 +0530

    Completed methods in the Patients class

--- VetDatabaseProject/Source.cpp
+++ VetDatabaseProject/Source.cpp
@@ -5,5 +5,13 @@ using namespace std;
 
 int main()
 {
+	Patients testPatient("Shunal", "Biscuit", "Dog");
+	testPatient.setVaccine("Rabies", "12th December, 2021");
+	testPatient.setVaccine("Parvo", "13th January, 2022");
+
+	testPatient.setHistory("None", "None", true);
+
+	testPatient.getPetHistory();
+
 	return 0;
 }
